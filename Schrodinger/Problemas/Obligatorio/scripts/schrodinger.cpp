b7cbbeed2aced6123418d6a5aa9b66fa176e8d45
Santiago <santivallecillos180@gmail.com>
2026-05-13 12:35:45 +0200

    Actualizo la carpeta de Schrodinger

---
+++ Schrodinger/Problemas/Obligatorio/scripts/schrodinger.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <complex>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
